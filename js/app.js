c3ee1eed5662cecbaa469e877a077fc672e4f829
Thar Nyi Phyo <konyi.tharnyiphyo@gmail.com>
2024-10-10 00:25:41 +0630

    Upade code

--- js/app.js
+++ js/app.js
@@ -36,3 +36,30 @@ body.addEventListener("click" , e =>{
     }
 });
 
+
+
+  // Get modal element
+  var modal = document.getElementById("signupModal");
+
+  // Get the button that opens the modal
+  var openModalBtn = document.getElementById("openModalBtn");
+
+  // Get the <span> element that closes the modal
+  var closeModalBtn = document.getElementById("closeModalBtn");
+
+  // When the user clicks the button, open the modal
+  openModalBtn.onclick = function() {
+      modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  closeModalBtn.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
